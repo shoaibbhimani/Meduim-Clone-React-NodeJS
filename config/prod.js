3aa3c4c4ee31fc12c75682ddb38e2154dfb6dbb6
shoaibbhimani <shoaibbhimani1392@gmail.com>
2017-12-25 16:54:42 +0530

    remove googleid from pd

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,4 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   token: process.env.TOKEN
 };
